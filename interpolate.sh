43e1dbe87d529ab3e1503c7fbc75cc4b45f3b97e
Otavio Bonder <otaviobonder@gmail.com>
2023-04-17 19:39:46 -0300

    chore: add path to script

--- interpolate.sh
+++ interpolate.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-node index.js
+# Get the directory path of the current script
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+# Specify the relative path to index.js
+INDEX_JS_PATH="$SCRIPT_DIR/index.js"
+
+node "$INDEX_JS_PATH"
 sleep 120
-node index.js --coin USDT --currency BRL
+node "$INDEX_JS_PATH" --coin USDT --currency BRL
